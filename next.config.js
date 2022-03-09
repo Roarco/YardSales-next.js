9d250c620db04527c97015225d919a3287023b75
Roarco <sebuscaroberth@gmail.com>
2022-03-09 14:10:52 -0500

    terminando las configuraciones basicas para un proyecto en next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    curstomKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
